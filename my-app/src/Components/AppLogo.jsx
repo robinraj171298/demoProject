799a7ec8f224eb654d1dc4523c25c95dbe16879c
robin raj token <robin.raj@amnhealthcare.com>
2022-10-23 20:12:04 +0530

    Updated folder structure

--- my-app/src/Components/AppLogo.jsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from "@mui/material";
-import { ReactComponent as BrandLogo } from "../Assets/Images/BrandLogo.svg"; 
-
-export const AppLogo = () => {
-    return(
-        <BrandLogo />
-    )
-};
