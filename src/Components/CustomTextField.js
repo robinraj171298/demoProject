1a1a832439bd4de9fda07c36dd7525cb69f01b95
robin raj token <robin.raj@amnhealthcare.com>
2022-10-25 16:17:21 +0530

    Updated styles

--- src/Components/CustomTextField.js
+++ src/Components/CustomTextField.js
@@ -1,24 +1,46 @@
 import { TextField } from '@mui/material';
 import './CustomTextField.css';
+import { Grid } from "@mui/material";
 
 export const CustomTextField = (props) => {
     return (
-        <>
-            <div class="inputLabel">
-                <label>{props.label}</label>
-            </div>
-            <TextField
-              class="customTextField"
-              InputProps={{ sx: { width: '100%' } }}
-              id={props.id}
-              //label={props.label}
-              placeholder={props.placeholder}
-              value={props.workspaceName}
-              onChange={(e) => props.onChange(e.target.value)}
-              minLength={props.minLength}
-              maxLength={props.maxLength}
-            />
-    </>
-  )
+      <>
+        <div class="inputLabel">
+          <label>{props.label}</label>
+        </div>
+        {props.id == "workspaceURL" ? (
+          <Grid container>
+            <Grid item>
+              <div class="workspaceUrlStyle">
+                <span class="workspaceUrlText">www.eden.com/</span>
+              </div>
+            </Grid>
+            <Grid item>
+              <TextField
+                class="customTextField"
+                InputProps={{ sx: { width: "100%" } }}
+                id={props.id}
+                placeholder={props.placeholder}
+                value={props.workspaceName}
+                onChange={(e) => props.onChange(e.target.value)}
+                minLength={props.minLength}
+                maxLength={props.maxLength}
+              />
+            </Grid>
+          </Grid>
+        ) : (
+          <TextField
+            class="customTextField"
+            InputProps={{ sx: { width: "100%" } }}
+            id={props.id}
+            placeholder={props.placeholder}
+            value={props.workspaceName}
+            onChange={(e) => props.onChange(e.target.value)}
+            minLength={props.minLength}
+            maxLength={props.maxLength}
+          />
+        )}
+      </>
+    );
 }
 
