3cc491ee71bd4448a9fe0c069544ceb596923a16
robin raj token <robin.raj@amnhealthcare.com>
2022-10-25 17:24:17 +0530

    Restructured folder structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { OnboardingSceen } from './Components/OnboardingScreen';
+import logo from "./logo.svg";
+import "./App.css";
+import { OnboardingSceen } from "./Components/Onboarding/OnboardingScreen";
 
 export const App = () => {
   return (
     <div class="App-outerContainer">
       <OnboardingSceen />
     </div>
-  )
-}
+  );
+};
 
 export default App;
