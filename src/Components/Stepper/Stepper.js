3a3fcf1fbd309f60da15dad0c6bc7a7ae5bbf40a
robin raj token <robin.raj@amnhealthcare.com>
2022-10-25 19:07:29 +0530

    Fixed issue

--- src/Components/Stepper/Stepper.js
+++ src/Components/Stepper/Stepper.js
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import "./Stepper.css";
 
 const steps = [1, 2, 3, 4];
 
